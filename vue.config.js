730e1362a680aaa1d4329c7d6b5dcfd53bc9ae46
Lantianzhi <1437802926@qq.com>
2024-05-20 22:42:05 +0800

    补充

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {  // 跨域代理
       '/api': {
-        target: 'http://127.0.0.1:8888/', // 需要代理的域名
+        target: 'http://127.0.0.1:8888/', // 代理的域名
         changeOrigin: true, // 开启代理，本地创建虚拟服务端，发送or接收请求
         pathRewrite: {
           // 重写匹配字段，如果不需要在请求路径上，重写为""
